40f9323a28905703fbe9a3a9ed2c2e8ce96c8a1d
spekary <spekary@gmail.com>
2024-12-31 03:52:58 -0800

    Export command

---
+++ _test/export/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/goradd/orm/_test/gen/orm/goradd"
+	"github.com/goradd/orm/pkg/config"
+	"github.com/goradd/orm/pkg/db"
+	_ "github.com/goradd/orm/tmpl/template"
+	"os"
+)
+
+func main() {
+	var configFile string
+	var outFile string
+
+	flag.StringVar(&configFile, "c", "", "Path to database configuration file")
+	flag.StringVar(&outFile, "o", "", "Path to output file")
+
+	flag.Parse()
+
+	if configFile == "" {
+		_, _ = fmt.Fprintf(os.Stderr, "Path to database configuration file is required")
+		os.Exit(1)
+	} else if outFile == "" {
+		_, _ = fmt.Fprintf(os.Stderr, "Path to output file is required")
+		os.Exit(1)
+	}
+
+	export(configFile, outFile)
+}
+
+func export(dbConfigFile, outFile string) {
+	if databaseConfigs, err := config.OpenConfigFile(dbConfigFile); err != nil {
+		panic(err)
+	} else if err := config.InitDatastore(databaseConfigs); err != nil {
+		panic(err)
+	}
+	ctx := db.NewContext(nil)
+
+	f, err := os.Create(outFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	err = goradd.JsonEncodeAll(ctx, f)
+	if err != nil {
+		panic(err)
+	}
+}
