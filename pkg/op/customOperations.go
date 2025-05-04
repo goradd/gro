c526c59deb9b6f3ab9bfcea20a0206ad6dbe9a24
spekary <spekary@gmail.com>
2025-05-04 15:02:01 -0700

    comment

--- pkg/op/customOperations.go
+++ pkg/op/customOperations.go
@@ -10,6 +10,9 @@ func EndsWith(arg1 any, arg2 string) *OperationNode {
 	return NewOperationNode(OpEndsWith, arg1, arg2)
 }
 
+// Contains returns on operation node that will return true for the following situations:
+//   - A text type field contains the given string value
+//   - An enum array node contains the given value
 func Contains(arg1, arg2 any) *OperationNode {
 	return NewOperationNode(OpContains, arg1, arg2)
 }
