40b377467e222fcec1376b286cba9da116020e5d
spekary <spekary@gmail.com>
2025-05-05 13:27:49 -0700

    Lock tests

---
+++ _test/ci/crud/forward_unl_test.go
@@ -0,0 +1,131 @@
+package crud
+
+import (
+	"github.com/goradd/orm/_test/gen/orm/goradd_unit"
+	"github.com/goradd/orm/_test/gen/orm/goradd_unit/node"
+	"github.com/goradd/orm/pkg/db"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+// TestForwardUniqueNullableLock tests insert and update of two linked records where the link is nullable.
+func TestForwardUniqueNullableLock(t *testing.T) {
+	ctx := db.NewContext(nil)
+	defer goradd_unit.ClearAll(ctx)
+
+	// Insert-insert
+	l1 := goradd_unit.NewLeafUnl()
+	r1 := goradd_unit.NewRootUnl()
+	r1.SetName("root")
+	l1.SetName("leaf")
+	l1.SetRootUnl(r1)
+	require.NoError(t, l1.Save(ctx))
+
+	// loading back
+	l1b, err := goradd_unit.LoadLeafUnl(ctx, l1.ID(), node.LeafUnl().RootUnl())
+	require.NoError(t, err)
+	assert.Equal(t, "leaf", l1b.Name())
+	assert.Equal(t, "root", l1b.RootUnl().Name())
+
+	// Update-update
+	l1.SetName("leaf2")
+	l1.RootUnl().SetName("root2")
+	err = l1.Save(ctx)
+	assert.NoError(t, err)
+	l1b, err = goradd_unit.LoadLeafUnl(ctx, l1.ID(), node.LeafUnl().RootUnl())
+	require.NoError(t, err)
+	assert.Equal(t, "leaf2", l1b.Name())
+	assert.Equal(t, "root2", l1b.RootUnl().Name())
+
+	// Insert-update
+	l2 := goradd_unit.NewLeafUnl()
+	l2.SetName("leaf3")
+	r1.SetName("root3")
+	l2.SetRootUnl(r1)
+	err = l2.Save(ctx)
+	assert.Error(t, err)
+	assert.IsType(t, &db.UniqueValueError{}, err)
+
+	// Update-insert
+	r3 := goradd_unit.NewRootUnl()
+	l1.SetName("leaf4")
+	r3.SetName("root4")
+	l1.SetRootUnl(r3)
+	err = l1.Save(ctx)
+	require.NoError(t, err)
+	l1b, err = goradd_unit.LoadLeafUnl(ctx, l1.ID(), node.LeafUnl().RootUnl())
+	require.NoError(t, err)
+	assert.Equal(t, "leaf4", l1b.Name())
+	assert.Equal(t, "root4", l1b.RootUnl().Name())
+}
+
+// TestForwardNullableCollision tests saving two records that are changed at the same time.
+func TestForwardUniqueNullableLockCollision(t *testing.T) {
+	ctx := db.NewContext(nil)
+	defer goradd_unit.ClearAll(ctx)
+
+	l := goradd_unit.NewLeafUnl()
+	r := goradd_unit.NewRootUnl()
+	r.SetName("root")
+	l.SetName("leaf")
+	l.SetRootUnl(r)
+	err := l.Save(ctx)
+	require.NoError(t, err)
+
+	var l2 *goradd_unit.LeafUnl
+	l2, err = goradd_unit.LoadLeafUnl(ctx, l.ID(), node.LeafUnl().RootUnl())
+	require.NoError(t, err)
+
+	// Update both
+	l.SetName("leaf2")
+	l2.SetName("leaf3")
+
+	err = l.Save(ctx)
+	err2 := l2.Save(ctx)
+	assert.NoError(t, err)
+	assert.Error(t, err2)
+	assert.IsType(t, &db.OptimisticLockError{}, err2)
+
+	// 2nd level
+	l2, _ = goradd_unit.LoadLeafUnl(ctx, l.ID(), node.LeafUnl().RootUnl())
+	l.RootUnl().SetName("root2")
+	l2.RootUnl().SetName("root3")
+
+	err = l.Save(ctx)
+	err2 = l2.Save(ctx)
+	assert.NoError(t, err)
+	assert.Error(t, err2)
+	assert.IsType(t, &db.OptimisticLockError{}, err2)
+}
+
+func TestForwardUniqueNullableLockNull(t *testing.T) {
+	ctx := db.NewContext(nil)
+	defer goradd_unit.ClearAll(ctx)
+	l := goradd_unit.NewLeafUnl()
+	l.SetName("leaf")
+	assert.NoError(t, l.Save(ctx))
+
+	l.SetRootUnl(nil) // nullable
+	assert.NoError(t, l.Save(ctx))
+}
+
+func TestForwardUniqueNullableLockTwo(t *testing.T) {
+	ctx := db.NewContext(nil)
+	defer goradd_unit.ClearAll(ctx)
+	r := goradd_unit.NewRootUnl()
+	l := goradd_unit.NewLeafUnl()
+	r.SetName("root")
+	l.SetName("leaf")
+	r.SetLeafUnl(l)
+	require.NoError(t, r.Save(ctx))
+
+	l2 := goradd_unit.NewLeafUnl()
+	l2.SetName("leaf2")
+	r.SetLeafUnl(l2)
+	require.NoError(t, r.Save(ctx)) // unique failure
+
+	l2, err := goradd_unit.LoadLeafUnl(ctx, l.ID())
+	require.NoError(t, err)
+	assert.NotNil(t, l2) // reverse linked item that is allowed to have a nil pointer was not deleted
+}
---
+++ _test/ci/crud/reverse_unl_test.go
@@ -0,0 +1,148 @@
+package crud
+
+import (
+	"github.com/goradd/orm/_test/gen/orm/goradd_unit"
+	"github.com/goradd/orm/_test/gen/orm/goradd_unit/node"
+	"github.com/goradd/orm/pkg/db"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+// TestReverseUniqueNullableLock tests insert and update of two linked records.
+func TestReverseUniqueNullableLock(t *testing.T) {
+	ctx := db.NewContext(nil)
+	defer goradd_unit.ClearAll(ctx)
+
+	// Insert-insert
+	r := goradd_unit.NewRootUnl()
+	l := goradd_unit.NewLeafUnl()
+	r.SetName("root")
+	l.SetName("leaf")
+	r.SetLeafUnl(l)
+	err := r.Save(ctx)
+	require.NoError(t, err)
+
+	var r2 *goradd_unit.RootUnl
+	r2, err = goradd_unit.LoadRootUnl(ctx, r.ID(), node.RootUnl().LeafUnl())
+	require.NoError(t, err)
+	assert.Equal(t, "root", r2.Name())
+	assert.Equal(t, "leaf", r2.LeafUnl().Name())
+
+	// Update-update
+	r.SetName("root2")
+	r.LeafUnl().SetName("leaf2")
+	err = r.Save(ctx)
+	assert.NoError(t, err)
+	r2, err = goradd_unit.LoadRootUnl(ctx, r.ID(), node.RootUnl().LeafUnl())
+	require.NoError(t, err)
+	assert.Equal(t, "root2", r2.Name())
+	assert.Equal(t, "leaf2", r2.LeafUnl().Name())
+
+	// Insert-update
+	r3 := goradd_unit.NewRootUnl()
+	r3.SetName("root3")
+	l.SetName("leaf3")
+	r3.SetLeafUnl(l)
+	err = r3.Save(ctx)
+	require.NoError(t, err)
+	r2, err = goradd_unit.LoadRootUnl(ctx, r3.ID(), node.RootUnl().LeafUnl())
+	require.NoError(t, err)
+	assert.Equal(t, "root3", r2.Name())
+	assert.Equal(t, "leaf3", r2.LeafUnl().Name())
+
+	// Update-insert
+	l4 := goradd_unit.NewLeafUnl()
+	r.SetName("root4")
+	l4.SetName("leaf4")
+	r.SetLeafUnl(l4)
+	err = r.Save(ctx)
+	require.NoError(t, err)
+	r2, err = goradd_unit.LoadRootUnl(ctx, r.ID(), node.RootUnl().LeafUnl())
+	require.NoError(t, err)
+	assert.Equal(t, "root4", r2.Name())
+	assert.Equal(t, "leaf4", r2.LeafUnl().Name())
+
+}
+
+// TestReverseUniqueNullableLockCollision tests saving two records that are changed at the same time.
+func TestReverseUniqueNullableLockCollision(t *testing.T) {
+	ctx := db.NewContext(nil)
+	defer goradd_unit.ClearAll(ctx)
+
+	r := goradd_unit.NewRootUnl()
+	l := goradd_unit.NewLeafUnl()
+	r.SetName("root")
+	l.SetName("leaf")
+	r.SetLeafUnl(l)
+	err := r.Save(ctx)
+	require.NoError(t, err)
+
+	var r2 *goradd_unit.RootUnl
+	r2, err = goradd_unit.LoadRootUnl(ctx, r.ID(), node.RootUnl().LeafUnl())
+	require.NoError(t, err)
+
+	r.SetName("root2")
+	r2.SetName("root3")
+
+	err = r.Save(ctx)
+	err2 := r2.Save(ctx)
+	assert.NoError(t, err)
+	assert.Error(t, err2)
+	assert.IsType(t, &db.OptimisticLockError{}, err2)
+
+	// Level 2
+	r2, _ = goradd_unit.LoadRootUnl(ctx, r.ID(), node.RootUnl().LeafUnl())
+
+	r.LeafUnl().SetName("leaf2")
+	r2.LeafUnl().SetName("leaf3")
+	err = r.Save(ctx)
+	err2 = r2.Save(ctx)
+	assert.NoError(t, err)
+	assert.Error(t, err2)
+	assert.IsType(t, &db.OptimisticLockError{}, err2)
+}
+
+func TestReverseUniqueNullableLockNull(t *testing.T) {
+	ctx := db.NewContext(nil)
+	defer goradd_unit.ClearAll(ctx)
+
+	r := goradd_unit.NewRootUnl()
+	r.SetName("root")
+	l := goradd_unit.NewLeafUnl()
+	l.SetName("leaf")
+	r.SetLeafUnl(l)
+	require.NoError(t, r.Save(ctx))
+
+	r.SetLeafUnl(nil)
+	require.NoError(t, r.Save(ctx))
+
+	r2, err := goradd_unit.LoadRootUnl(ctx, r.ID(), node.RootUnl().LeafUnl())
+	require.NoError(t, err)
+	assert.Nil(t, r2.LeafUnl())
+
+	l2, err := goradd_unit.LoadLeafUnl(ctx, l.ID())
+	require.NoError(t, err)
+	assert.NotNil(t, l2) // reverse linked item that could have a nil pointer was not deleted
+	assert.Nil(t, l2.RootUnl())
+}
+
+func TestReverseUniqueNullableLockTwo(t *testing.T) {
+	ctx := db.NewContext(nil)
+	defer goradd_unit.ClearAll(ctx)
+	r := goradd_unit.NewRootUnl()
+	l := goradd_unit.NewLeafUnl()
+	r.SetName("root")
+	l.SetName("leaf")
+	r.SetLeafUnl(l)
+	require.NoError(t, r.Save(ctx))
+
+	l2 := goradd_unit.NewLeafUnl()
+	l2.SetName("leaf2")
+	r.SetLeafUnl(l2)
+	require.NoError(t, r.Save(ctx)) // unique failure
+
+	// Confirm detached.
+	l3, _ := goradd_unit.LoadLeafUnl(ctx, l.ID())
+	assert.Nil(t, l3.RootUnl())
+}
